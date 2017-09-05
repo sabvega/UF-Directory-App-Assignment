59337a631334922d1113c0d63485407829842b97
Sabrina Vega <sabrina.alicia24@gmail.com>
2017-09-04 21:08:47 -0400

    added required functionality

--- listingController.js
+++ listingController.js
@@ -2,13 +2,43 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.show = false;
+    $scope.newCode = "";
+    $scope.newName = "";
+    $scope.newLat = "";
+    $scope.newLong = "";
+    $scope.newAdd = "";
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
-  }
+    $scope.addListing = function() {
+      $scope.listings.push({
+        "code": $scope.newCode,
+        "name": $scope.newName,
+        "coordinates": {
+          "latitude": $scope.newLat,
+          "longitude": $scope.newLong
+        },
+        "address": $scope.newAdd
+      });
+     
+      //reset values 
+      $scope.newCode = "";
+      $scope.newName = "";
+      $scope.newLat = "";
+      $scope.newLong = "";
+      $scope.newAdd = "";
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+      $scope.show=false;
+    };
+    $scope.showDetails = function(index) {
+       $scope.detailedInfo = 
+        $scope.listings[index];
+      $scope.show = true;
+    };
+  }s
 ]);
