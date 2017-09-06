610cc91bdbae774ed7037d0b0c92ca842648a0d3
Sabrina Vega <sabrina.alicia24@gmail.com>
2017-09-05 22:49:26 -0400

    added uri and schema

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,9 +7,10 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config.js');
 
 /* Connect to your database */
+  mongoose.connect(config.db);
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
